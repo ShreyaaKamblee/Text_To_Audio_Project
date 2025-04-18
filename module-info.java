27c0cbf06833e4358bd09b1a79fe6c4f97c3cda7
ShreyaaKamblee <144820425+ShreyaaKamblee@users.noreply.github.com>
2025-04-18 12:52:11 +0530

    module-info.java

---
+++ module-info.java
@@ -0,0 +1,9 @@
+module com.example.texttospeech {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires freetts;
+
+
+    opens com.example.texttospeech to javafx.fxml;
+    exports com.example.texttospeech;
+}
